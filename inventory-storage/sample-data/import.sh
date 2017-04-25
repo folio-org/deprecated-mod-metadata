7cd64a2cf194546df05ae585356383e48b19f018
Julian Ladisch <julianladisch@users.noreply.github.com>
2017-04-25 13:39:47 +0200

    DMOD-245 loan type endpoint

--- inventory-storage/sample-data/import.sh
+++ inventory-storage/sample-data/import.sh
@@ -3,7 +3,16 @@
 tenant=${1:-demo_tenant}
 item_storage_address=http://localhost:9130/item-storage/items
 instance_storage_address=http://localhost:9130/instance-storage/instances
+loan_type_storage_address=http://localhost:9130/loan-types
 material_type_storage_address=http://localhost:9130/material-types
+
+for f in ./loantypes/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -d @$f \
+         "${loan_type_storage_address}"
+done
 
 for f in ./mtypes/*.json; do
     curl -w '\n' -X POST -D - \
@@ -11,7 +20,6 @@ for f in ./mtypes/*.json; do
          -H "X-Okapi-Tenant: ${tenant}" \
          -d @$f \
          "${material_type_storage_address}"
-
 done
 
 for f in ./instances/*.json; do
